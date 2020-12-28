434a388d530b316d7522137dc38efe8be5c7f401
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-28 11:24:27 +0900

    Movie Graphql API

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,18 @@
-import { getById, people } from './db';
+import { getById as peopleGetById, people } from './db';
+import { getById, movies, addMovie, deleteMovie } from './movieData';
 
 const resolvers = {
   Query: {
     people: () => people,
-    person: (_, {id} ) => getById(id)
+    person: (_, {id} ) => peopleGetById(id),
+    movies: () => movies,
+    movie: (_, {id}) => getById(id)
+  },
+  Mutation: {
+    deleteMovie: (_, { id }) => deleteMovie(id),
+    addMovie: (_, { name, score }) => addMovie(name,score)
   }
 }
 
   
-export default resolvers; 
+export default resolvers;  
