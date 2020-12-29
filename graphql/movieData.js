6bf9cf63d5b772dadefe60a41698068cbf4e4af6
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-29 22:25:01 +0900

    node-fetch -> axios

--- graphql/movieData.js
+++ graphql/movieData.js
@@ -1,5 +1,9 @@
 import fetch from "node-fetch";
-const API_URL = "https://yts.am/api/v2/list_movies.json?";
+import axios from "axios";
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAIL_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
 export let movies = [
     {
@@ -44,9 +48,25 @@ export const addMovie = (name, score) => {
     return newMovie;
 }
 
-export const getMovies = (limit, rating) => {
-    console.log("getMovies");
-    let REQUEST_URL = API_URL;
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: {movies}
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  });
+  return movies;
+};
+
+
+// axios 와 fetch
+/*
+  export const getMovies = (limit, rating) => {
+    let REQUEST_URL = LIST_MOVIES_URL;
     if (limit > 0) {
         REQUEST_URL += `limit=${limit}`;
     }
@@ -57,3 +77,4 @@ export const getMovies = (limit, rating) => {
         .then(res => res.json())
         .then(json => json.data.movies);
   };
+  */
